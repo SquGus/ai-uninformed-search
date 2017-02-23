28dc74deb2493d4a03921406e7744145df8856da
SquGus <squgus@gmail.com>
2017-02-23 00:40:08 -0600

    Trial and error

--- search_ia.py
+++ search_ia.py
@@ -1,13 +1,21 @@
 # https://www.alphagrader.com/courses/6/assignments/11
 import re
 
-def input_to_stacks(input_string):
+def input_to_state(input_string):
+	stacks = ()
+	for stack_string in input_string.split('; '):
+		stacks += (stack_string, )
+	return stacks
+
+def input_to_stacks(state_array):
 	stacks = []
-	for stack_string in input_string.split(';'):
-		stacks.append([ box for box in re.sub(r'[ ()]','',stack_string).split(',')])
-	for i,stack in enumerate(stacks):
-		if stack[0] == 'X':
-			stacks[i] = 'X'
+	for i, box in enumerate(state_array):
+		stacks.append([])
+		box = box.replace("(", "")
+		box = box.replace(")", "")
+		box = box.replace(" ", "")
+		for item in box.split(","):
+			stacks[i].append(item)
 	return stacks
 
 def move_box(current,neu_position,stacks):
@@ -33,47 +41,57 @@ def test_goal(stacks,goal_stacks):
 	return valid
 
 def expand_nodes(max_height,stacks,goal_stacks):
-	visited = set()
-	initial_stacks = (stacks,0)
+	visited = {}
+	initial_stacks = (stacks, 0)
 	dfs_stack = [initial_stacks]
 
 	while True:
 		if len(dfs_stack) == 0:
-			return False #por elmomento
+			return False #por el momento
 		current_state = dfs_stack.pop()
+		print(current_state)
+
 		current_stack = current_state[0]
-		if test_goal(current_stack,goal_stacks):
+		print(current_stack)
+		
+		if test_goal(current_stack, goal_stacks):
 			return True
-		visited.add(current_stack)
+		visited[current_state] = current_stack
 
 		for i,box in enumerate(current_stack):
 			for j in len(current_stack):
 				if i != j and len(current_stack[j]) < max_height:
 					neu_stacks, cost = move_box(i,j,current_stack)
-					visited.add(neu_stacks)
+					visited[(neu_stacks, cost)] = (neu_stacks)
 					dfs_stack.append((neu_stacks,cost+current_state[1]))
 
 
 max_height = int(input())
-stacks = input_to_stacks(input())
-stacks_original = list(stacks)
-goal_stacks = input_to_stacks(input())
+initial_state = input_to_state(input())
+goal_state = input_to_state(input())
+stacks = input_to_stacks(goal_state)
 
+print(initial_state)
+print(goal_state)
 
-expand_nodes(max_height,stacks,goal_stacks)
+for stack in goal_state:
+	print(stack)
+# stacks_original = list(stacks)
+
+# expand_nodes(max_height, stacks, goal_stacks)
 
 # graph = {stacks:[]}
 
-#FALTA CREAR LA VUSQUEDA QUE PARA CADA STACK INTENTE MOVER A LOS OTROS STACKS
-search = []
+#FALTA CREAR LA BUSQUEDA QUE PARA CADA STACK INTENTE MOVER A LOS OTROS STACKS
+# search = []
 # actions for for
-movements = []
+# movements = []
 
 
 
 		
 
-print(stacks)
+# print(stacks)
 # print(cost)
 # print(stacks)
 # print(goal_stacks)

08b0d94152f89431d0aeb097ce827ac349714ef3
erikiado <erikiado@gmail.com>
2017-02-23 11:01:04 -0600

    Creo que la busqueda esta terminada, la copie y cambie por queue para obtener bfs, falta solucionar la historia, revisar que si funcione bien la busqueda y las busquedas de A*

--- search_ia.py
+++ search_ia.py
@@ -1,5 +1,7 @@
 # https://www.alphagrader.com/courses/6/assignments/11
 import re
+import time
+import copy
 
 def input_to_stacks(input_string):
 	stacks = []
@@ -11,7 +13,7 @@ def input_to_stacks(input_string):
 	return stacks
 
 def move_box(current,neu_position,stacks):
-	neu_stacks = list(stacks)
+	neu_stacks = stacks[:]
 	cost = 0.5 + abs(current - neu_position) + 0.5
 	box = neu_stacks[current].pop()
 	neu_stacks[neu_position].append(box)
@@ -32,26 +34,85 @@ def test_goal(stacks,goal_stacks):
 					valid = False
 	return valid
 
-def expand_nodes(max_height,stacks,goal_stacks):
+def test_in_list(stack,stacks):
+	for x in stacks:
+		if test_goal(stack,x):
+			return True
+	return False
+
+
+def check_better_cost(stacks, cost, queue):
+	valid = False
+	not_in_list = True
+	for state in queue:
+		if test_goal(stacks,state[0]):
+			not_in_list = False
+			if cost < state[1]:
+				valid = True
+
+	if not_in_list:
+		return True
+	else:
+		return valid
+
+
+
+def dfs(max_height,stacks,goal_stacks):
 	visited = set()
 	initial_stacks = (stacks,0)
 	dfs_stack = [initial_stacks]
-
+	stacks = [stacks]
 	while True:
 		if len(dfs_stack) == 0:
-			return False #por elmomento
+			return 'No solution found'
 		current_state = dfs_stack.pop()
-		current_stack = current_state[0]
-		if test_goal(current_stack,goal_stacks):
-			return True
-		visited.add(current_stack)
-
+		current_stack = stacks.pop()
+		visited.add(str(current_stack))
 		for i,box in enumerate(current_stack):
-			for j in len(current_stack):
-				if i != j and len(current_stack[j]) < max_height:
-					neu_stacks, cost = move_box(i,j,current_stack)
-					visited.add(neu_stacks)
-					dfs_stack.append((neu_stacks,cost+current_state[1]))
+			for j in range(len(current_stack)):
+				if i != j and len(current_stack[j]) < max_height and len(current_stack[i]) > 0:
+					current_stack_copy = copy.deepcopy(current_stack)
+					neu_stacks, cost = move_box(i,j,current_stack_copy)
+					if str(neu_stacks) not in visited or not test_in_list(neu_stacks, stacks):
+						if test_goal(neu_stacks,goal_stacks):
+							return neu_stacks, cost+current_state[1]
+						if check_better_cost(neu_stacks,cost+current_state[1], dfs_stack):
+							dfs_stack.append((neu_stacks,cost+current_state[1]))
+							stacks.append(neu_stacks)
+
+
+def bfs(max_height,stacks,goal_stacks):
+	visited = set()
+	initial_stacks = (stacks,0)
+	bfs_q = [initial_stacks]
+	queue = [stacks]
+	while True:
+		if len(bfs_q) == 0:
+			return 'No solution found'
+		current_state = bfs_q.pop()
+		current_stack = queue.pop()
+		visited.add(str(current_stack))
+		for i,box in enumerate(current_stack):
+			for j in range(len(current_stack)):
+				if i != j and len(current_stack[j]) < max_height and len(current_stack[i]) > 0:
+					# copy to not change the original list
+					current_stack_copy = copy.deepcopy(current_stack) 
+					neu_stacks, cost = move_box(i,j,current_stack_copy)
+					if str(neu_stacks) not in visited or not test_in_list(neu_stacks, queue):
+						if test_goal(neu_stacks,goal_stacks):
+							return neu_stacks, cost+current_state[1]
+						if check_better_cost(neu_stacks,cost+current_state[1], bfs_q):
+
+
+
+
+							#############################################################################
+							#############################################################################
+							# Agregar el padre del stack, para hacer una lista de estados como historia #
+							#############################################################################
+							#############################################################################
+							bfs_q.insert(0,(neu_stacks,cost+current_state[1]))
+							queue.insert(0,neu_stacks)
 
 
 max_height = int(input())
@@ -60,24 +121,5 @@ stacks_original = list(stacks)
 goal_stacks = input_to_stacks(input())
 
 
-expand_nodes(max_height,stacks,goal_stacks)
-
-# graph = {stacks:[]}
-
-#FALTA CREAR LA VUSQUEDA QUE PARA CADA STACK INTENTE MOVER A LOS OTROS STACKS
-search = []
-# actions for for
-movements = []
-
-
-
-		
-
-print(stacks)
-# print(cost)
-# print(stacks)
-# print(goal_stacks)
-# print(test_goal(stacks,goal_stacks))
-# cost = 
-
-# append y pop
+# falta guardar el historial
+print(bfs(max_height,stacks,goal_stacks))
